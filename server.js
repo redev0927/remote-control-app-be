f908b5c4b3084c22ad4356cd151a900c19e155e6
redev0927 <redev0927@gmail.com>
2024-09-08 01:27:37 +0900

    Create Project Structure

---
+++ server.js
@@ -0,0 +1,8 @@
+// node_modules 에 있는 express 관련 파일을 가져온다.
+var express = require("express");
+// express 는 함수이므로, 반환값을 변수에 저장한다.
+var app = express();
+// 3000 포트로 서버 오픈
+app.listen(5000, function () {
+  console.log("start! express server on port 5000");
+});
